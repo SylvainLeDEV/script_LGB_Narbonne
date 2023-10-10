ab11b1655d98059ae67ac788ec58a338a42ee993
SylvainLeDev <dev.tess.sylvain@gmail.com>
2023-10-10 11:12:58 +0200

    add loop + phase 2 with day

--- src/script.js
+++ src/script.js
@@ -29,33 +29,55 @@ updateTimestamp(targetDate);
 // --- Function for scrap lgb --
 async function startScrapping() {
   try {
-    // ## -- Connection to LGB -- ##
-    const browser = await puppeteer.launch({
-      headless: false,
-      devtools: true,
-    });
-    const page = await browser.newPage();
+    while (true) {
+      // Boucle infinie
+      // ## -- Connection to LGB -- ##
+      const browser = await puppeteer.launch({
+        // headless: false,
+        // devtools: true,
+      });
+      const page = await browser.newPage();
 
-    await connectToLGB(page, DATA_TOKEN);
+      await connectToLGB(page, DATA_TOKEN);
 
-    // ## -- Select dorp down menu : Phase 1 -- ##
-    await selectDropdownMenus(page, CHOICE_FOR_DINER);
+      // ## -- Select dorp down menu : Phase 1 -- ##
+      await selectDropdownMenus(page, CHOICE_FOR_DINER);
 
-    // ## -- Check month : Phase 1 -- ##
-    await checkMonth(page, CHOICE_FOR_DINER);
+      // ## -- Check month : Phase 1 -- ##
+      await checkMonth(page, CHOICE_FOR_DINER);
 
-    // ##-- Check Button Disabled or not : Phase 1 --##
-    await checkDayAvailable(page, CHOICE_FOR_DINER);
+      // ##-- Check Button Disabled or not : Phase 1 --##
+      await checkDayAvailable(page, CHOICE_FOR_DINER, 1);
 
-    // ## -- Click on Continue : Phase 1 -- ##
-    await clickContinueButton(page);
+      // ## -- Click on Continue : Phase 1 -- ##
+      await clickContinueButton(page);
+      const currentDate = new Date();
 
-    // ##-- Check Button Disabled or not : Phase 2 --##
-    await checkDayAvailable(page, CHOICE_FOR_DINER);
+      // ##-- Check Button Disabled or not : Phase 2 --##
+      if (await checkDayAvailable(page, CHOICE_FOR_DINER, 2)) {
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleString("fr-FR", {
+          day: "numeric",
+          month: "numeric",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        console.log(`Date trouvée à ${formattedDate}. Arrêt de la recherche.`);
+        break; // Arrête la boucle lorsque la bonne date est trouvée
+      }
 
-    console.log("Script Lunched think close ctr + C");
+      console.log(
+        "One iteration completed. Waiting before the next iteration..."
+      );
+
+      // Fermez le navigateur et attendez un certain temps avant de recommencer
+      // await browser.close();
+      // await new Promise((resolve) => setTimeout(resolve, 10000)); // Attendez 60 secondes (1 minute) avant de continuer la prochaine itération
+    }
   } catch (err) {
-    console.error("An error occurred :", err);
+    console.error("An error occurred:", err);
   }
 }
+
 startScrapping();
